19b66540ce29191d0fcc1736af746b5091ddbcd0
sushiljainam <sushiljain1993@gmail.com>
2016-12-07 18:08:47 +0530

    button-plus added, user configs set

--- tt-edit-app.js
+++ tt-edit-app.js
@@ -13,9 +13,28 @@ ttEditApp.controller('inputTableCntr', function inputTableCntr($scope) {
   ];
   $scope.classesEachDay = [
   {"label":"I","startTime":"","endTime":""},
-  {"label":"I","startTime":"","endTime":""},
-  {"label":"I","startTime":"","endTime":""},
-  {"label":"I","startTime":"","endTime":""},
-  {"label":"I","startTime":"","endTime":""}
+  {"label":"II","startTime":"","endTime":""},
+  {"label":"III","startTime":"","endTime":""},
+  {"label":"IV","startTime":"","endTime":""},
+  {"label":"V","startTime":"","endTime":""}
   ]
+
+  $scope.userConfigs = {};
+  $scope.userConfigs.subjects = [
+  {"key":"sub1"},
+  {"key":"sub2"},
+  {"key":"sub3"},
+  {"key":"sub4"},
+  {"key":"sub5"},
+  {"key":"sub6"}
+  ];
+
+  $scope.userConfigs.rooms = [
+  {"key":"room1"},
+  {"key":"room2"},
+  {"key":"room3"},
+  {"key":"room4"},
+  ];
+
+
 });
