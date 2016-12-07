103b334f16936a8c60b42627e817259b378b8ded
sushiljainam <sushiljain1993@gmail.com>
2016-12-07 18:52:27 +0530

    rows html css done, now fxns

--- tt-edit-app.js
+++ tt-edit-app.js
@@ -38,3 +38,64 @@ ttEditApp.controller('inputTableCntr', function inputTableCntr($scope) {
 
 
 });
+
+
+// Define the `inputListCntr` controller on the `ttEditApp` module
+ttEditApp.controller('inputListCntr', function inputListCntr($scope) {
+  $scope.dayItems = [
+  {"label":"Mon"},
+  {"label":"Tues"},
+  {"label":"Wednes"},
+  {"label":"Thurs"},
+  {"label":"Fri"},
+  {"label":"Satur"}
+  ];
+  $scope.classesEachDay = [
+  {"label":"I","startTime":"","endTime":""},
+  {"label":"II","startTime":"","endTime":""},
+  {"label":"III","startTime":"","endTime":""},
+  {"label":"IV","startTime":"","endTime":""},
+  {"label":"V","startTime":"","endTime":""}
+  ]
+
+  $scope.userConfigs = {};
+  $scope.userConfigs.subjects = [
+  {"key":"sub1"},
+  {"key":"sub2"},
+  {"key":"sub3"},
+  {"key":"sub4"},
+  {"key":"sub5"},
+  {"key":"sub6"}
+  ];
+
+  $scope.userConfigs.rooms = [
+  {"key":"room1"},
+  {"key":"room2"},
+  {"key":"room3"},
+  {"key":"room4"},
+  ];
+
+  $scope.userConfigs.teachers = [
+  {"key":"tea1"},
+  {"key":"tea2"},
+  {"key":"tea3"},
+  {"key":"tea4"},
+  ];
+
+  $scope.userConfigs.batches = [
+  {"label":"cs", "many" : [1,2,3,4]},
+  {"label":"ee", "many" : [1,2,3,4,5,6]},
+  {"label":"ce", "many" : [1,2,3,4,5,6,7,8]}
+  ];
+
+  $scope.userConfigs.t = {};
+  $scope.userConfigs.t.btc = 0;
+
+  
+  $scope.userConfigs.rows = [];
+
+  $scope.addRow = function () {
+    
+  }
+
+});
