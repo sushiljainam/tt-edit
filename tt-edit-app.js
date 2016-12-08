b676ec9d723ad91e3685ab746c89c0932c9d2699
sushiljainam <sushiljain1993@gmail.com>
2016-12-08 17:26:55 +0530

    routing DONE - more - change Loc

--- tt-edit-app.js
+++ tt-edit-app.js
@@ -33,37 +33,58 @@ ttEditApp.controller('bodyCntr', ['$scope','$cookies','$location', function inpu
   }
 
   //code for routing with path or search or hash
-  var decision = $location.hash() ? 2 : 1; console.log($location.hash(),$location.path(),$location.search());
-  switch(decision){
-    case 2:
-    var arr = $location.hash().split('/');
-    console.log(arr);
-    break;
+  $scope.urlToData = function () {
+    var decision = $location.hash() ? 2 : 1; console.log($location.hash(),$location.path(),$location.search());
+    switch(decision){
+      case 2:
+      var arr = $location.hash().split('/');
+      console.log(arr);
+      break;
+      
+      default:
+      var arr = $location.path().split('/'); //$location.search().split('/')
+      console.log(arr);
+      arr.splice(0,1);
+      console.log(arr);
+      break;
+  
+    }
+      var input1 = arr[0];
+      var input2 = arr[1];
+    console.log('input1,input2',input1,input2);
+  
+    var reg = new RegExp('^[0-9]+$');
+    if (reg.test(input2)) {var semCh = input2; var brCh = input1;};
+    // console.log('sem,br',semCh,brCh);
+    if (reg.test(input1)) {var semCh = input1; var brCh = input2;};
+    // console.log('sem,br',semCh,brCh);
+  
+    if (!(semCh && brCh)) {var brCh = input1;};
+    console.log('sem,br',semCh,brCh);
+  
+    $scope.semCh = semCh; $scope.brCh = brCh;
+    if(reg.test($scope.brCh)) $scope.brCh = undefined;
     
-    default:
-    var arr = $location.path().split('/'); //$location.search().split('/')
-    console.log(arr);
-    arr.splice(0,1);
-    console.log(arr);
-    break;
-
+    $scope.conf = {};
+    $scope.conf.semCh = angular.copy($scope.semCh);
+    $scope.conf.brCh = angular.copy($scope.brCh);
   }
-    var input1 = arr[0];
-    var input2 = arr[1];
-  console.log('input1,input2',input1,input2);
 
-  var reg = new RegExp('^[0-9]+$');
-  if (reg.test(input2)) {var semCh = input2; var brCh = input1;};
-  // console.log('sem,br',semCh,brCh);
-  if (reg.test(input1)) {var semCh = input1; var brCh = input2;};
-  // console.log('sem,br',semCh,brCh);
+  //end
 
-  if (!(semCh && brCh)) {var brCh = input1;};
-  console.log('sem,br',semCh,brCh);
 
-  $scope.semCh = semCh; $scope.brCh = brCh;
-  if(reg.test($scope.brCh)) $scope.brCh = undefined;
+  $scope.changeLoc = function () {
+    console.log('in loc');
+    $location.path('/'+ $scope.conf.semCh+'/'+ $scope.conf.brCh);
+    $scope.urlToData();
+  }
 
+  //code for save and saving
+  $scope.savePost = function(){
+    console.log('saving data');
+  }
+
+  $scope.urlToData();
 }]);
 
 
