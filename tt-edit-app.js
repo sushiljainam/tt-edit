4c2c73d88fc6a222b5257b44e1ae56fefc5ffffb
sushiljainam <sushiljain1993@gmail.com>
2016-12-08 16:21:58 +0530

    routing DONE- with path and hash

--- tt-edit-app.js
+++ tt-edit-app.js
@@ -2,7 +2,7 @@
 var ttEditApp = angular.module('ttEditApp', ['ngCookies']);
 
 // Define the `bodyCntr` controller on the `ttEditApp` module
-ttEditApp.controller('bodyCntr', ['$scope','$cookies', function inputTableCntr($scope, $cookies) {
+ttEditApp.controller('bodyCntr', ['$scope','$cookies','$location', function inputTableCntr($scope, $cookies, $location) {
 
   $scope.dayItems = [
   {"label":"Monday"},
@@ -31,6 +31,38 @@ ttEditApp.controller('bodyCntr', ['$scope','$cookies', function inputTableCntr($
     // console.log('3',$cookies.get('viewTable'),typeof $scope.viewTable, $scope.viewTable);
 
   }
+
+  //code for routing with path or search or hash
+  var decision = $location.hash() ? 2 : 1; console.log($location.hash(),$location.path(),$location.search());
+  switch(decision){
+    case 2:
+    var arr = $location.hash().split('/');
+    console.log(arr);
+    break;
+    
+    default:
+    var arr = $location.path().split('/'); //$location.search().split('/')
+    console.log(arr);
+    arr.splice(0,1);
+    console.log(arr);
+    break;
+
+  }
+    var input1 = arr[0];
+    var input2 = arr[1];
+  console.log('input1,input2',input1,input2);
+
+  var reg = new RegExp('^[0-9]+$');
+  if (reg.test(input2)) {var semCh = input2; var brCh = input1;};
+  // console.log('sem,br',semCh,brCh);
+  if (reg.test(input1)) {var semCh = input1; var brCh = input2;};
+  // console.log('sem,br',semCh,brCh);
+
+  if (!(semCh && brCh)) {var brCh = input1;};
+  console.log('sem,br',semCh,brCh);
+
+  $scope.semCh = semCh; $scope.brCh = brCh;
+  if(reg.test($scope.brCh)) $scope.brCh = undefined;
 
 }]);
 
