1c5658be0b677d3648acf18af3c192b9cf33ae9f
sushiljainam <sushiljain1993@gmail.com>
2016-12-09 12:08:00 +0530

    directive-start and animate

--- tt-edit-app.js
+++ tt-edit-app.js
@@ -1,5 +1,5 @@
 // Define the `ttEditApp` module
-var ttEditApp = angular.module('ttEditApp', ['ngCookies']);
+var ttEditApp = angular.module('ttEditApp', ['ngCookies','ngAnimate']);
 
 // Define the `bodyCntr` controller on the `ttEditApp` module
 ttEditApp.controller('bodyCntr', ['$scope','$http','$cookies','$location', function inputTableCntr($scope, $http, $cookies, $location) {
@@ -23,7 +23,7 @@ ttEditApp.controller('bodyCntr', ['$scope','$http','$cookies','$location', funct
 	$scope.viewTable = $cookies.get('viewTable') ? (JSON.parse($cookies.get('viewTable')) === true ? true : false) : false;
 	$scope.toggleViewTable = function () {
 		// console.log('1',$cookies.get('viewTable'),typeof $scope.viewTable, $scope.viewTable);
-		
+
 		$cookies.put('viewTable',(!$scope.viewTable).toString());
 		// console.log('2',$cookies.get('viewTable'),typeof $scope.viewTable, $scope.viewTable);
 
@@ -40,31 +40,31 @@ ttEditApp.controller('bodyCntr', ['$scope','$http','$cookies','$location', funct
 			var arr = $location.hash().split('/');
 			console.log(arr);
 			break;
-			
+
 			default:
 			var arr = $location.path().split('/'); //$location.search().split('/')
 			console.log(arr);
 			arr.splice(0,1);
 			console.log(arr);
 			break;
-	
+
 		}
 			var input1 = arr[0];
 			var input2 = arr[1];
 		console.log('input1,input2',input1,input2);
-	
+
 		var reg = new RegExp('^[0-9]+$');
 		if (reg.test(input2)) {var semCh = input2; var brCh = input1;};
 		// console.log('sem,br',semCh,brCh);
 		if (reg.test(input1)) {var semCh = input1; var brCh = input2;};
 		// console.log('sem,br',semCh,brCh);
-	
+
 		if (!(semCh && brCh)) {var brCh = input1;};
 		console.log('sem,br',semCh,brCh);
-	
+
 		$scope.semCh = semCh; $scope.brCh = brCh;
 		if(reg.test($scope.brCh)) $scope.brCh = undefined;
-		
+
 		$scope.conf = {};
 		$scope.conf.semCh = angular.copy($scope.semCh);
 		$scope.conf.brCh = angular.copy($scope.brCh);
@@ -79,7 +79,7 @@ ttEditApp.controller('bodyCntr', ['$scope','$http','$cookies','$location', funct
 		$scope.urlToData();
 	}
 
-	
+
 	//code for save and saving
 	$scope.savingPost = false;
 	$scope.savePost = function(){
@@ -124,5 +124,3 @@ ttEditApp.controller('bodyCntr', ['$scope','$http','$cookies','$location', funct
 
 	$scope.urlToData();
 }]);
-
-
